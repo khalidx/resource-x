8ac7a1b34adb941135ea4a7d90629b0820848391
Khalid Zoabi <kzoabi@outlook.com>
2019-07-06 02:30:41 -0400

    Adding support for colored log output

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,7 @@
 import fse from 'fs-extra'
 import path from 'path'
 import program from 'commander'
+import chalk from 'chalk'
 import debug from 'debug'
 import inquirer from 'inquirer'
 import figlet from 'figlet'
@@ -14,6 +15,17 @@ import * as rx from './index'
 
 const debugCli = debug('rx:cli')
 debugCli.log = console.info.bind(console)
+
+/**
+ * Logs to the console with colorized output
+ * @param message the data to log 
+ */
+export const log = (type: 'info' | 'message' | 'success' | 'error', message: any) => {
+  if (type === 'message') console.log(chalk.yellow(message))
+  else if (type === 'success') console.log(chalk.green(message))
+  else if (type === 'error') console.log(chalk.red(message))
+  else debugCli(chalk.italic.yellow(message))
+}
 
 /**
  * Initializes a new sample project in the specified directory.
